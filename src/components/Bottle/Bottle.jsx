fd26bbf31ac52f1b874f67330bd46dc12e9d2d4f
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-07 18:40:12 +0600

    fetch data from json and load data

---
+++ src/components/Bottle/Bottle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Bottle = ({ bottle }) => {
+//   console.log(bottle);
+
+// const {img}
+
+  return (
+    <div>
+      <img src={bottle.img} alt="" />
+    </div>
+  );
+};
+
+export default Bottle;
---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,19 @@
+import React, { use } from "react";
+import Bottle from "../Bottle/Bottle";
+
+const Bottles = ({ bottlesPromise }) => {
+  const bottles = use(bottlesPromise);
+
+  //   console.log(bottles)
+
+  return (
+    <div>
+      <h3>Bottles: {bottles.length}</h3>
+      {bottles.map((bottle) => (
+        <Bottle key={bottle.id} bottle={bottle}></Bottle>
+      ))}
+    </div>
+  );
+};
+
+export default Bottles;
