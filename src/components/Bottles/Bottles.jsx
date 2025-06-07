baabf1f9a5de24928f5061ef6243ae962b04ccc9
MaishaTahsin <rubaitreshad@gmail.com>
2025-06-07 22:27:08 +0600

    implemented remove from cart using local storage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,23 +1,61 @@
-import React, { use, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import "./Bottles.css";
+import Cart from "../Cart/cart";
 import Bottle from "../Bottle/bottle";
+import {
+  addToStoredCart,
+  getStoreCart,
+  removeFromCart,
+} from "../../utilities/localStorage";
 
 const Bottles = ({ bottlesPromise }) => {
   const [cart, setCart] = useState([]);
   const bottles = use(bottlesPromise);
 
+  //useEffect
+  useEffect(() => {
+    const storedCartIDs = getStoreCart();
+    // console.log(storedCartIDs);
+
+    const storedCart = [];
+
+    for (const id of storedCartIDs) {
+      //   console.log(id);
+
+      const cartBottle = bottles.find((bottle) => bottle.id === id);
+
+      if (cartBottle) {
+        storedCart.push(cartBottle);
+      }
+    }
+
+    setCart(storedCart);
+  }, [bottles]);
+
   const handleAddToCart = (bottle) => {
     // console.log("Bottles will be added to the cart", bottle);
     const newCart = [...cart, bottle];
     setCart(newCart);
+
+    // save the bottle id in the storage
+    addToStoredCart(bottle.id);
   };
 
+  const handleRemoveFromCart = (id) => {
+    console.log("remove item form the cart", id);
+
+    const remainingCart = cart.filter((bottle) => bottle.id !== id);
+    setCart(remainingCart);
+    removeFromCart(id);
+  };
   //   console.log(bottles)
 
   return (
     <div>
       <h3>Bottles: {bottles.length}</h3>
       <p>Added to cart: {cart.length}</p>
+
+      <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
 
       <div className="bottles-container">
         {bottles.map((bottle) => (
